ff4159b0e609729c11f3baaa94322a958ad93a39
mrzhuangda <qq3221278@163.com>
2016-11-14 17:28:03 +0800

    files of bookstore

---
+++ js/controllers.js
@@ -0,0 +1,141 @@
+/**
+ * 这里是书籍列表模块
+ * @type {[type]}
+ */
+var bookListModule = angular.module("BookListModule", []);
+bookListModule.controller('BookListCtrl', function($scope, $http, $state, $stateParams) {
+    $scope.filterOptions = {
+        filterText: "",
+        useExternalFilter: true
+    };
+    $scope.totalServerItems = 0;
+    $scope.pagingOptions = {
+        pageSizes: [5, 10, 20],
+        pageSize: 5,
+        currentPage: 1
+    };
+    $scope.setPagingData = function(data, page, pageSize) {
+        var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+        $scope.books = pagedData;
+        $scope.totalServerItems = data.length;
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    };
+
+    //这里可以根据路由上传递过来的bookType参数加载不同的数据
+    // console.log($stateParams);
+    $scope.getPagedDataAsync = function(pageSize, page, searchText) {
+        setTimeout(function() {
+            var data;
+            if (searchText) {
+                var ft = searchText.toLowerCase();
+                $http.get('data/books' + $stateParams.bookType + '.json')
+                    .success(function(largeLoad) {
+                        data = largeLoad.filter(function(item) {
+                            return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                        });
+                        $scope.setPagingData(data, page, pageSize);
+                    });
+            } else {
+                $http.get('data/books' + $stateParams.bookType + '.json')
+                    .success(function(largeLoad) {
+                        $scope.setPagingData(largeLoad, page, pageSize);
+                    });
+            }
+        }, 100);
+    };
+
+    $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+
+    $scope.$watch('pagingOptions', function(newVal, oldVal) {
+        if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
+    $scope.$watch('filterOptions', function(newVal, oldVal) {
+        if (newVal !== oldVal) {
+            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
+
+    $scope.gridOptions = {
+        data: 'books',
+        // rowTemplate: '<div style="height: 100%"><div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell ">' +
+        //     '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }"> </div>' +
+        //     '<div ng-cell "></div>' +
+        //     '</div></div>',          默认也有的
+        multiSelect: false,
+        enableCellSelection: true,
+        enableRowSelection: false,
+        enableCellEdit: true,
+        enablePinning: true,
+        columnDefs: [{
+            field: 'index',
+            displayName: '序号',
+            width: 60,
+            pinnable: false,
+            sortable: false
+        }, {
+            field: 'name',
+            displayName: '书名',
+            enableCellEdit: true
+        }, {
+            field: 'author',
+            displayName: '作者',
+            enableCellEdit: true,
+            width: 220
+        }, {
+            field: 'pubTime',
+            displayName: '出版日期',
+            enableCellEdit: true,
+            width: 120
+        }, {
+            field: 'price',
+            displayName: '定价',
+            enableCellEdit: true,
+            width: 120,
+            cellFilter: 'currency:"￥"'  //默认的是美元符号
+        }, {
+            field: 'bookId',
+            displayName: '操作',
+            enableCellEdit: false,
+            sortable: false,
+            pinnable: false,
+            cellTemplate: '<div><a ui-sref="bookdetail({bookId:row.getProperty(col.field)})" id="{{row.getProperty(col.field)}}">详情</a></div>'
+        }],
+        enablePaging: true,
+        showFooter: true,
+        totalServerItems: 'totalServerItems',
+        pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions
+    };
+});
+
+
+/**
+ * 这里是书籍详情模块
+ * @type {[type]}
+ */
+var bookDetailModule = angular.module("BookDetailModule", []);
+bookDetailModule.controller('BookDetailCtrl', function($scope, $http, $state, $stateParams) {
+    //请模仿上面的代码，用$http到后台获取数据，把这里的例子实现完整
+       $http({
+        method:'GET',
+        url:'data/books.json'
+       }).success(function(data){
+                   angular.forEach(data,function(value,index){   
+                if($stateParams.bookId== index){
+                        $scope.name=value.name;
+                        $scope.time=value.pubTime;
+                        $scope.typ=value.typ;
+                        $scope.author=value.author;
+                        $scope.price=value.price;
+                }
+               })
+       }).error(function(){
+          alert('有错误');
+       })
+
+        
+});
